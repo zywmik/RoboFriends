bd24f41ec8333501b9b02b614f954547d573ec3b
Mikolaj Zywczok <mikolaj.zywczok@ericsson.com>
2019-01-17 12:35:14 +0100

    Updates

---
+++ precache-manifest.f4b7d27f41ef9d126a369b813dd1b2c3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/RoboFriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "877cac2a9c44d7342a3d",
+    "url": "/RoboFriends/static/js/runtime~main.877cac2a.js"
+  },
+  {
+    "revision": "fa71ddb7ad23dea202c7",
+    "url": "/RoboFriends/static/js/main.fa71ddb7.chunk.js"
+  },
+  {
+    "revision": "34a45851498f2cae25a5",
+    "url": "/RoboFriends/static/js/1.34a45851.chunk.js"
+  },
+  {
+    "revision": "fa71ddb7ad23dea202c7",
+    "url": "/RoboFriends/static/css/main.de307ac1.chunk.css"
+  },
+  {
+    "revision": "34a45851498f2cae25a5",
+    "url": "/RoboFriends/static/css/1.b2dec56c.chunk.css"
+  },
+  {
+    "revision": "51b43fd30e9d020219dff4e10dd4877d",
+    "url": "/RoboFriends/index.html"
+  }
+];
