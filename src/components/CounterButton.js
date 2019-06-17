77a3283c295c8818d21ccad6b588bf5d31287914
Mikolaj Zywczok <mikolaj.zywczok@gmail.com>
2019-06-17 10:00:17 +0200

    ...

--- src/components/CounterButton.js
+++ src/components/CounterButton.js
@@ -1,9 +1,9 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 class CounterButton extends Component {
   constructor(props) {
     super(props);
-    this.state = {count: 0};
+    this.state = { count: 5 };
   }
   shouldComponentUpdate(nextProps, nextState) {
     if (this.state.count !== nextState.count) {
@@ -18,11 +18,12 @@ class CounterButton extends Component {
       <button
         id='counter'
         color={this.props.color}
-        onClick={() => this.setState(state => ({count: state.count + 1}))}>
+        onClick={() => this.setState((state) => ({ count: state.count + 1 }))}
+      >
         Count: {this.state.count}
       </button>
     );
   }
 }
 
-export default CounterButton
+export default CounterButton;
