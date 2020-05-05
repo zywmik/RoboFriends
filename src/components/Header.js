5ed822a43a1646ae72e206a7918d235ae9501885
Mikolaj Zywczok <mikolaj.zywczok@gmail.com>
2020-05-05 12:11:28 +0200

    update readme, remove <CounterButton /> from header, add border radius to <SearchBox />

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,13 @@
-import React, {Component} from 'react';
-import CounterButton from './CounterButton';
+import React, { PureComponent } from "react";
 
-// In most cases, instead of writing shouldComponentUpdate()
-// by hand, you can inherit from React.PureComponent. It is
-// equivalent to implementing shouldComponentUpdate() with a
-// shallow comparison of current and previous props and state.
-
-class Header extends Component {
-  shouldComponentUpdate(nextProps, nextState) {
-    return false
-  }
+class Header extends PureComponent {
   render() {
     return (
       <div>
-        <h1 className='f1'>RoboFriends</h1>
-        <CounterButton color={'red'} />
+        <h1 className="f1">RoboFriends</h1>
       </div>
     );
   }
-};
+}
 
 export default Header;
